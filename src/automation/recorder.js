1c8911f57c02ef775456185d8bcc8f6287f0f135
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-04-24 18:03:58 -0400

    refactored recorder as a function (class adds unecessary complexity)

--- src/automation/recorder.js
+++ src/automation/recorder.js
@@ -5,52 +5,55 @@ const Store = require('electron-store')
 /**
  * Record peripheral input events and save in readable format
  */
-class Recorder {
-    constructor(botName) {
-        // Retrieve store
-        const store = new Store()
+const record = async (botName) => {
+    // Retrieve store
+    const store = new Store()
 
-        // Retrieve bot (create default bot if doesn't exist)
-        this.bot = store.get(botName)
-        if (!this.bot) {
-            this.bot = {
-                variables: [],
-                steps: []
-            }
+    // Retrieve bot (create default bot if doesn't exist)
+    const bot = store.get(botName)
+    if (!this.bot) {
+        this.bot = {
+            variables: [],
+            steps: []
         }
-
-        // Register and start hook (debugging disabled)
-        ioHook.start(false)
     }
 
-    async record() {
-        // Mouse click event
-        ioHook.on('mouseclick', (event) => {
-            this.bot.steps.append(event)
-        })
+    // Register and start hook (debugging disabled)
+    ioHook.start(false)
 
-        // Mouse drag event
-        ioHook.on('mousedrag', (event) => {
-            this.bot.steps.append(event)
-        })
+    // Mouse click event
+    ioHook.on('mouseclick', (event) => {
+        // Save to bot
+        bot.steps.append(event)
+    })
 
-        // Mouse wheel event
-        ioHook.on('mousewheel', (event) => {
-            this.bot.steps.append(event)
-        })
+    // Mouse drag event
+    ioHook.on('mousedrag', (event) => {
+        // Save to bot
+        bot.steps.append(event)
+    })
 
-        // Key down event
-        ioHook.on('keydown', (event) => {
-            // TODO: Save bot and exit if 'esc' pressed
-            this.bot.steps.append(event)
-        })
+    // Mouse wheel event
+    ioHook.on('mousewheel', (event) => {
+        // Save to bot
+        bot.steps.append(event)
+    })
 
-        // Key up event
-        ioHook.on('keyup', (event) => {
-            this.bot.steps.append(event)
-        })
-    }
+    // Key down event
+    ioHook.on('keydown', (event) => {
+        // TODO: Save bot and exit if 'esc' pressed
+        store.set(botName, bot)
+
+        // Save to bot
+        bot.steps.append(event)
+    })
+
+    // Key up event
+    ioHook.on('keyup', (event) => {
+        // Save to bot
+        bot.steps.append(event)
+    })
 }
 
 // Export
-module.exports = Recorder
+module.exports = record
