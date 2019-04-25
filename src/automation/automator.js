7cc14f522e027b0b062dbd125448fb22086e8a41
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2019-04-25 00:43:13 -0400

    cleaned dragMouse event

--- src/automation/automator.js
+++ src/automation/automator.js
@@ -16,7 +16,7 @@ const record = async (botName) => {
     if (!bot) {
         bot = {
             variables: [],
-            steps: []
+            events: []
         }
     }
 
@@ -37,60 +37,78 @@ const record = async (botName) => {
 
         // Create clean event
         const cleanEvent = {
-            event: 'mouseMove->mouseClick',
+            eventName: 'mouseMove->mouseClick',
             button: mouseButtonMapping[event.button],
             x: mouse.x,
-            y: mouse.y
+            y: mouse.y,
+            double: null
         }
 
         // Check if previous click pos was same as this one (=> double click)
-        const prevEvent = bot.steps[bot.steps.length - 1]
+        const prevEvent = bot.events[bot.events.length - 1]
         if (prevEvent && prevEvent.event === 'mouseMove->mouseClick' && !prevEvent.double
                 && prevEvent.button === cleanEvent.button
                 && prevEvent.x === cleanEvent.x && prevEvent.y === cleanEvent.y) {
-            // Save only secondEvent (as double click)
-            cleanEvent.double = true
-            bot.steps.pop()
+            // Modify prevEvent to be double click
+            bot.events[bot.events.length - 1].double = true
         } else {
             // Save firstEvent and secondEvent (as single click)
             cleanEvent.double = false
         }
 
         // Save to bot
-        bot.steps.push(cleanEvent)
-        console.log(cleanEvent)
+        bot.events.push(cleanEvent)
     })
 
     // Mouse drag event
-    ioHook.on('mousedrag', (event) => {
-        // Save to bot
-        console.log(event)
-        bot.steps.push(event)
+    ioHook.on('mousedrag', () => {
+        // Get mouse coordinates according to robotJs
+        const mouse = robot.getMousePos()
+
+        // Create clean event
+        const cleanEvent = {
+            eventName: 'dragMouse',
+            fromX: mouse.x,
+            fromY: mouse.y,
+            toX: null,
+            toY: null
+        }
+
+        // Check if prevEvent was dragMouse
+        const prevEvent = bot.events[bot.events.length - 1]
+        if (prevEvent && prevEvent.eventName === 'dragMouse') {
+            // Modify toX and toY of prevEvent to current position
+            bot.events[bot.events.length - 1].toX = mouse.x
+            bot.events[bot.events.length - 1].toY = mouse.y
+        } else {
+            // Save cleaned version of event to bot
+            bot.events.push(cleanEvent)
+        }
     })
 
     // // Mouse wheel event
     // ioHook.on('mousewheel', (event) => {
     //     // Save to bot
     //     console.log(event)
-    //     bot.steps.push(event)
+    //     bot.events.push(event)
     // })
 
     // Key down event
     ioHook.on('keydown', (event) => {
         // TODO: Save bot and exit if 'esc' pressed
         // store.set(botName, bot)
-        console.log(event)
+        console.log(bot)
         // return true
 
         // Save to bot
-        bot.steps.push(event)
+        bot.events.push(event)
     })
 
     // Key up event
     ioHook.on('keyup', (event) => {
         // Save to bot
         console.log(event)
-        bot.steps.push(event)
+        bot.events.push(event)
     })
 }
 
